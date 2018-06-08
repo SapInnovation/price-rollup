bea5205c43266211d97ec58c7152562139b222fa
hitesh <hgupta@sapient.com>
2018-06-08 11:28:15 +0530

    Updates build to create docker image

--- src/main/java/com/sapient/retail/price/rollup/controller/PriceRollUpConfiguration.java
+++ src/main/java/com/sapient/retail/price/rollup/controller/PriceRollUpConfiguration.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.Serdes.StringSerde;
 import org.apache.kafka.streams.Consumed;
@@ -23,8 +25,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerde;
+import org.springframework.util.StringUtils;
 
 import com.sapient.retail.price.rollup.model.Price;
 import com.sapient.retail.price.rollup.model.ProductPrice;
@@ -34,9 +38,11 @@ import com.sapient.retail.price.rollup.model.SKUPrice;
 @Configuration
 @EnableKafka
 @EnableKafkaStreams
+
 public class PriceRollUpConfiguration {
 	
 	@Autowired private KafkaProperties kafkaProperties;
+	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Value (value = "${kafka.intopic}")
@@ -44,6 +50,27 @@ public class PriceRollUpConfiguration {
 	
 	@Value (value = "${kafka.outtopic}")
 	private String priceKafkaOutTopic;
+	
+	@Value (value = "${kafka.server}")
+	private List<String> priceKafkaServer;
+	
+	@Bean
+	public KafkaAdmin admin() {
+	    Map<String, Object> configs = new HashMap<>();
+	    configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,
+	            StringUtils.collectionToCommaDelimitedString(priceKafkaServer));
+	    return new KafkaAdmin(configs);
+	}
+
+	@Bean
+	public NewTopic topic1() {
+	    return new NewTopic(priceKafkaInTopic, 1, (short) 1);
+	}
+
+	@Bean
+	public NewTopic topic2() {
+	    return new NewTopic(priceKafkaOutTopic, 1, (short) 1);
+	}
 
 	@SuppressWarnings( "resource")
 	@Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
@@ -51,7 +78,7 @@ public class PriceRollUpConfiguration {
 		logger.info("Setting Kafka configuration : StreamsConfig ");
         Map<String, Object> props = new HashMap<>();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "price-streams");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, priceKafkaServer);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, StringSerde.class);
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, JsonSerde.class);
         props.put(JsonDeserializer.DEFAULT_KEY_TYPE, String.class);
